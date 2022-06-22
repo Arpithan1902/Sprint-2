675f7ac83ffcf2dace1edb620e1461be674bb31b
arpithan1902 <arpitha.n@capgemini.com>
2022-06-22 09:22:00 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import FetchAllUsers from './components/FetchAllUsers';
+import FetchUser from './components/FetchUser';
+
+import Dashboard from './components/Dashboard';
+import UpdateUser from './components/UpdateUser';
+import AddUser from './components/AddUser';
+
+import DeleteUser from './components/DeleteUser';
+import Home from './components/Home';
+import ForgetPassword from './components/ForgetPassword';
+import Login from './components/Login';
+
+import FetchBookingByUserId from './components/FetchBookingByUserId';
+import NavBarDashboard from './navbars/NavBarDashboard';
+import Logout from './components/Logout';
+import FetchAllCars from './components/FetchAllCars';
+import FetchAllBookings from './components/FetchAllBookings';
+
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       {/* <header className="App-header"> */}
+      
+        
+        
+        <Router>
+          <Routes>
+          <Route path="/" element={<Home/>}/>
+            <Route path="/dashboard" element={<Dashboard/>}/>
+            <Route path="/navbar" element={<NavBarDashboard/>}/> 
+            
+            
+            <Route path='/user/all' element={<FetchAllUsers/>}/>
+            <Route path='/user/get/:id' element={<FetchUser/>}/>
+            <Route path='/user/update/:id' element={<UpdateUser/>}/>
+            <Route path='/user/add' element={<AddUser/>}/>
+            <Route path='/login' element={<Login/>}/>
+            <Route path='/user/delete/:id' element={<DeleteUser/>}/>  
+            <Route path='/forgetPassword' element={<ForgetPassword/>}/>
+            <Route path='/user/booking/:id' element={<FetchBookingByUserId/>}/>
+            <Route path='/logout' element={<Logout/>}/>
+            <Route path='/car/all' element={<FetchAllCars/>}/>
+            <Route path='/booking/all' element={<FetchAllBookings/>}/>
+            
+           
+          </Routes>
+
+        </Router>
+        {/* </header> */}
+     
     </div>
   );
 }
